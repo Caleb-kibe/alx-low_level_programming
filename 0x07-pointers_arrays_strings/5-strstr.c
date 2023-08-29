73c130eeeb241ad985b1b7da4b2a73153d2e5379
caleb-kibe <kiplagatcaleb2@gmail.com>
2023-08-29 09:19:05 -0700

    locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,5 +2,25 @@
 
 /**
   * _strstr - function that locates a substring
-  * @haystack: 
+  * @haystack: input parameter
+  * @needle: input parameter
+  * Return: 0 (success)
+  */
+
 char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0' ; haystack++)
+	{
+		char *one = haystack;
+		char *two = needle;
+
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
+		}
+		if (*two == '\0')
+			return (haystack);
+	}
+	return (0);
+}
