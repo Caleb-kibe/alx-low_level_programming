c0213f9878193177d0d00ce5cf2660351281477d
caleb-kibe <kiplagatcaleb2@gmail.com>
2023-09-01 13:22:34 -0700

    number of arguements

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - main function
+  * @argc: argurment count
+  * @argv: arguement vector
+  * Return: 0 (success)
+  */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
