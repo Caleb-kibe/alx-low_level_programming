9411c08375aecfce00c0750e9a9aa2767b4aceb6
caleb-kibe <kiplagatcaleb2@gmail.com>
2023-09-13 14:06:29 -0700

    x0F. C - Function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - adds a and b
+ * op_sub - subtracts a and b
+ * op_mul - multiplies a and b
+ * op_div - divides a and b
+ * op_mod - returns the remainder of division of a and b
+ * @a: parameter 1
+ * @b: parameter 2
+ */
+
+int op_add(int a, int b)
+	return (a + b);
+
+int op_sub(int a, int b)
+	return (a - b);
+
+int op_mul(int a, int b)
+	return (a * b);
+
+int op_div(int a, int b)
+	return (a / b);
+
+int op_mod(int a, int b)
+	return (a % b);
+
