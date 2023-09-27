5f3e48e60beffaa03b3d3ab6709748d5ff4e275f
caleb-kibe <kiplagatcaleb2@gmail.com>
2023-09-27 17:46:28 +0300

    function that prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ * _puts_recursion - function that prints a string followed by a new line
+ * @s: string to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+}
