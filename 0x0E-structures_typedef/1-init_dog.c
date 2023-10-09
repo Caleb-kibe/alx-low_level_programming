25b371c1d151b0f9351d87a54eac7b3642dad222
caleb-kibe <kiplagatcaleb2@gmail.com>
2023-10-09 19:31:03 +0300

    initializes a variable that describes a dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * init_dog - function that describes a dog
+ * struct dog - structure that describes the dog
+ * @d: points to the dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		exit(98);
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
