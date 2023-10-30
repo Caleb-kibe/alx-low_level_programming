d35e425a596cc0cafa35fc2268d38198cb675503
caleb-kibe <kiplagatcaleb2@gmail.com>
2023-10-30 22:56:34 +0300

    updated header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define BUF_SIZE 1024
 
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char **argv);
+void error_98(int f0, char *buff, char *argv);
+void error_99(int f0, char *buff, char *argv);
+void error_100(int f0, char *buff);
 
 #endif
