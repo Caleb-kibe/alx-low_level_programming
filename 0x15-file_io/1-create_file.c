5348005abd66349f9f74ee576a09dce1fa5f7f89
caleb-kibe <kiplagatcaleb2@gmail.com>
2023-10-31 21:40:19 +0300

    made changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,23 +9,29 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file;
-	int output = 1, count = 0;
+	int fd;
+	int letters;
+	int fwrite;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (file == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
 		return (-1);
-	if (text_content)
-	{
-		while (text_content[count])
-			count++;
-		output = write(file, text_content, count);
-	}
-	if (output == -1)
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (letters = 0; text_content[letters]; letters++)
+		;
+
+	fwrite = write(fd, text_content, letters);
+
+	if (fwrite == -1)
 		return (-1);
-	close(file);
+
+	close(fd);
 	return (1);
 }
