67692d10578d61241c567444ad62febd0044f576
caleb kibe <kiplagatcaleb2@gmail.com>
2024-03-20 18:24:14 +0300

    finds the perimeter of an island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,16 +12,13 @@ def island_perimeter(grid):
         the perimter of the island
     """
 
-    width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
-
-    for i in range(height):
-        for j in range(width):
+    perimeter = 0
+    for i in range(0, len(grid), 1):
+        for j in range(0, len(grid[0]), 1):
             if grid[i][j] == 1:
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-
-    return size * 4 - edges * 2
+                perimeter = perimeter + 4
+                if j - 1 >= 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+                if i - 1 >= 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+    return (perimeter)
